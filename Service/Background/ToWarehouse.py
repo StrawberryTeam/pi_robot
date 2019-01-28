c1b8ce2ad49c64173673df0eb59e0941624556e7
Zack Lee <zacklees@hotmail.com>
2019-01-28 17:47:43 +0800

    warehouse video

--- Service/Background/ToWarehouse.py
+++ Service/Background/ToWarehouse.py
@@ -5,12 +5,17 @@ import json
 from Common.Straw import Straw
 from Common.Util import Util
 import shutil
+import subprocess
+import math
 
 
 class ToWarehouse(Straw):
     '''
     创建仓库，并上传本地文件至远程仓库
     文件大于单个文件数量时 分割文件
+        显示时长 s
+    ffprobe -i VEYCaSRIBFn.mp4 -show_format -v quiet | sed -n 's/duration=//p'
+        duration / filezise * maxsize 
     ffmpeg -i .\GxEfOYUhozv.mp4 -map 0 -c copy -f segment -segment_time 500 output_%03d.mp4
     '''
 
@@ -32,9 +37,9 @@ class ToWarehouse(Straw):
         size = os.path.getsize(os.path.join(self._config.TASK['fileDir'], args['file']))
         # warehouse 单个文件不能超过 45MB
         sizeMb = size / 1024 / 1024
-        if sizeMb > 45:
-            Util.error('{} 文件超过单个文件大小限制 size: {}MB'.format(args['file'], sizeMb))
-            return False
+        # if sizeMb > 45:
+        #     Util.error('{} 文件超过单个文件大小限制 size: {}MB'.format(args['file'], sizeMb))
+        #     return False
 
         settingInfo = self.getModel('Setting').getSetting(self._config.WAREHOUSE['uid'])
         if 'lastRepoId' not in settingInfo:
@@ -49,22 +54,55 @@ class ToWarehouse(Straw):
                 return False
 
         # 开始提交文件
-        originUrl = self.commitFiles(args['file'], lastRepoId)
-        Util.info('更新远程地址 {}'.format(originUrl))
+        fileList = self.commitFiles(args['file'], lastRepoId, sizeMb)
+        Util.info('更新远程地址 {}'.format(fileList))
         # 更新远程地址至 远程 UID
-        self.getModel('VideoList').newPlay(args['id'], self._config.WAREHOUSE['uid'], originUrl)
+        self.getModel('VideoList').newPlay(args['id'], self._config.WAREHOUSE['uid'], fileList)
         return True
 
+
+    _maxSize = 45 # max size MB
+    # 获取总时长
+    _getDuration = "ffprobe -i {} -show_format -v quiet"
+    # 开始切分
+    _doSegment = "ffmpeg -i {} -map 0 -c copy -f segment -segment_time {} {}_%03d.mp4"
     # 提交文件至仓库
-    def commitFiles(self, file, repoName):
+    def commitFiles(self, file, repoName, fileSize):
+        '''
+            file 待提交文件
+            repoName 待提交创建名称
+            fileSize 待提交文件大小 MB
+        '''
+
         os.chdir(os.path.join(self._config.TASK['repoDir'], repoName))
         os.system('git pull origin gh-pages')
-        shutil.copyfile(os.path.join(self._config.TASK['fileDir'], file), os.path.join(self._config.TASK['repoDir'], repoName, os.path.basename(file)))
+        fileList = []
+        # 超过单个文件大小限制的
+        if fileSize > self._maxSize:
+            fileDuration = subprocess.check_output(self._getDuration.format(os.path.join(self._config.TASK['fileDir'], file)))
+            fileDuration = fileDuration.decode('UTF-8')
+            fileDuration = fileDuration[fileDuration.find('duration=', 0) + 9:fileDuration.find('size=', fileDuration.find('duration=', 0))]
+            segmentTime = float(fileDuration) / float(fileSize) * self._maxSize
+            Util.info('Files duration')
+            Util.info(segmentTime, float(fileDuration),  float(fileSize), self._maxSize)
+            # 创建新的批量文件名 newFileName_000.mp4
+            newFileName = Util.genRandName(11)
+            os.system(self._doSegment.format(os.path.join(self._config.TASK['fileDir'], file), segmentTime, newFileName))
+            # 最大文件数
+            fileNum = math.ceil(float(fileDuration) / segmentTime)
+            # 确认文件列表
+            print('filenummmmm', fileNum)
+            for num in range(fileNum):
+                if True == os.path.isfile("%s_%03d.mp4" % (newFileName, num)): 
+                    fileList.append("https://%s/%s/%s_%03d.mp4" % (self._config.WAREHOUSE['host'], repoName, newFileName, num))
+        else:
+            shutil.copyfile(os.path.join(self._config.TASK['fileDir'], file), os.path.join(self._config.TASK['repoDir'], repoName, os.path.basename(file)))
+            fileList.append("https://{}/{}/{}".format(self._config.WAREHOUSE['host'], repoName, os.path.basename(file)))
         os.system('git add *.mp4 && git add *.jpg && git add *.png && git add *.jpeg')
         os.system('git commit -m {}'.format(os.path.basename(file)))
         os.system('git push origin gh-pages')
         Util.info("文件添加至仓库成功")
-        return "https://{}/{}/{}".format(self._config.WAREHOUSE['host'], repoName, os.path.basename(file))
+        return fileList
 
     # 创建仓库
     _apiCreateRepo = {
